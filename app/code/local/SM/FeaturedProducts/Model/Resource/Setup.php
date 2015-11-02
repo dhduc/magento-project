1269afc4c723b5fa10b2eaa253ae735c0c7c5b30
Michael Dao <huuduc.uneti@yahoo.com>
2015-11-02 21:41:29 +0700

    Module-1: Add attribute 'is_featured' for products

---
+++ app/code/local/SM/FeaturedProducts/Model/Resource/Setup.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * Class SM_FeaturedProducts_Model_Resource_Setup
+ */
+class SM_FeaturedProducts_Model_Resource_Setup extends Mage_Eav_Model_Entity_Setup
+{
+}
--- app/code/local/SM/FeaturedProducts/sql/sm_featuredproducts_setup/mysql4-install-0.1.0.php
+++ app/code/local/SM/FeaturedProducts/sql/sm_featuredproducts_setup/mysql4-install-0.1.0.php
@@ -2,9 +2,29 @@
 
 /** @var $installer Mage_Core_Model_Resource_Setup */
 $installer = $this;
-
+$setup = new SM_FeaturedProducts_Model_Resource_Setup('core_setup');
 $installer->startSetup();
 
-
+/**
+ * Add attribute 'is_featured' to products
+ */
+$setup->addAttribute('catalog_product', 'is_featured', array(
+        'group' => 'General',
+        'input' => 'boolean',
+        'type' => 'int',
+        'label' => 'Featured Product',
+        'backend' => '',
+        'visible' => 1,
+        'required' => 0,
+        'user_defined' => 1,
+        'searchable' => 1,
+        'filterable' => 1,
+        'comparable' => 1,
+        'visible_on_front' => 1,
+        'visible_in_advanced_search' => 0,
+        'is_html_allowed_on_front' => 0,
+        'global' => Mage_Catalog_Model_Resource_Eav_Attribute::SCOPE_GLOBAL,
+    )
+);
 
 $installer->endSetup();
